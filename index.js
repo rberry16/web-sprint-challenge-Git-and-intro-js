f51041bc20f596237c1519fae50405678a92924c
Ryan Berry <rberry16@hotmail.com>
2022-01-14 04:55:11 -0700

    first six tasks completed

--- index.js
+++ index.js
@@ -208,16 +208,17 @@ Practice accessing data above by console.log-ing following items:
 (no functions needed) */
 
 //(1) Name of the first artist (0th index) in the array
-
+// console.log(artists[0]);
 
 //(2) Bio of the third artist (2nd index) in the array 
-
+// console.log(artists[2].bio);
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 (no function needed) 
 There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Fix this issue and console.log() to check your work. */
-
+artists[8].name = 'Vincent Van Gogh';
+// console.log(artists[8].name);
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀  
@@ -228,9 +229,10 @@ Use getArtistByIndex to do the following:
 
 Example, if getArtistByIndex is invoked with the artists array and the number 0, it will return `the artist at index 0 is Amedeo Modigliani` */
 
-function getArtistByIndex(/*Your Code Here*/) {
-  /*Your Code Here*/
+function getArtistByIndex(array, number) {
+  return `the artist at index ${number} is ${array[number].name}`;
 }
+// console.log(getArtistByIndex(artists, 0));
 
 
 
@@ -243,10 +245,17 @@ Example born in 1901 and died in 1959 - included -- born in 1889 and died in 192
 If correct, the function should return ["Salvador Dali", "Frida Kahlo"]*/
 // Hint - Look up the .split() method
 
-function get20s(/*Your Code Here*/) {
-  /*Your Code Here*/
+function get20s(array) {
+  const the20s = [];
+  for (let i = 0; i < array.length; i++){
+    if (array[i].years.split(' ')[0] >= 1900 && array[i].years.split(' ')[2] < 2000){
+      the20s.push(array[i].name);
+    }
+  }
+  return the20s;
 }
-
+// console.log(get20s(artists));
+// console.log(artists[0].name)
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -258,8 +267,9 @@ Use removeArtist to do the following:
 
 For example, if removeArtist is invoked with the artists array and the number 0, it will remove Amedeo Modigliani from our dataset and return the number 19. */
 
-function removeArtist(/*Your Code Here*/) {
-  /*Your Code Here*/
+function removeArtist(array, number) {
+  array.splice(number, 1);
+  return array.length;
 }
 
 
@@ -279,9 +289,19 @@ Use addArtist to do the following:
 
 Example: addArtist(artists) should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/) {
-  /*Your Code Here*/
+function addArtist(array) {
+  const newArtist = {
+    id: 20,
+    name: 'Ryan Berry',
+    years: '1990 - current day',
+    genre: 'Web Design',
+    nationality: 'American',
+    bio: 'hot hot lava man. lava man hot hot'
+  };
+  array.push(newArtist);
+  return(array); 
 }
+// console.log(addArtist(artists));
 
 
 
